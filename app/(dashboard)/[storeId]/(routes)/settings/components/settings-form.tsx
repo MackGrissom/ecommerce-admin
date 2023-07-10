013361bc7dd36e8f67f2081c1e2acab544180edf
Mack Grissom <mcintyregrissom@gmail.com>
2023-07-10 18:01:45 +0700

    added separator

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,19 +1,41 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client"
+import { Trash } from "lucide-react";
 import React from "react";
+import { z } from "zod";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+
+const formSchema = z
+
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
   return (
-    <div>
-        Settings Form! 
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Settings"
+          description="Manage store preferences" />
+
+  <Button 
+  variant="destructive"
+  size="sm"
+  onClick={() => {}}
+    >
+      <Trash className="h-4 w-4"/> 
+  </Button>
+      </div>
+      <Separator /> 
+    </>
   )
 }
 
